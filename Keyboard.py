ae978a5f862dbbf1c459d6aa844b11d37d5ad83a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-06-15 22:18:55 -0500

    [.] Mouse, Keyboard: Define Clases que heredan de Device

---
+++ Keyboard.py
@@ -0,0 +1,22 @@
+from Device import Device
+
+class Keyboard( Device ) :
+
+    counter = 0
+
+    def __init__( self, type, brand ) :
+        Device .__init__( self, type, brand )
+        self .__id = self .increment_counter()
+
+    def __str__( self ) :
+        return f'{ self .__id } [ { Device .__str__( self ) } ]'
+
+    @classmethod
+    def increment_counter( cls ) :
+        cls .counter += 1
+
+        return cls .counter
+
+if __name__ == '__main__' :
+    keyboard_1 = Keyboard( 'Keyboard', 'Logitec' )
+    print( keyboard_1 )
---
+++ Mouse.py
@@ -0,0 +1,22 @@
+from Device import Device
+
+class Mouse( Device ) :
+
+    counter = 0
+
+    def __init__( self, type, brand ) :
+        Device .__init__( self, type, brand )
+        self .__id = self .increment_counter()
+
+    def __str__( self ) :
+        return f'{ self .__id } [ { Device .__str__( self ) } ]'
+
+    @classmethod
+    def increment_counter( cls ) :
+        cls .counter += 1
+
+        return cls .counter
+
+if __name__ == '__main__' :
+    mouse_1 = Mouse( 'Mouse', 'Microsoft' )
+    print( mouse_1 )
